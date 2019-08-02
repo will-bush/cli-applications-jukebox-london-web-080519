0c8c30f6e32232de15de803871efcb538081cd83
Will Bush <williamdcbush@gmail.com>
2019-08-02 09:58:12 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -28,8 +28,9 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   selection = gets.strip
-  if selection.to_i < songs.length
-    puts "Playing #{songs[selection -1]}"
+  if selection.to_i <= songs.length
+    puts "Playing #{songs[selection]}"
+    elsif selection 
   else 
     puts "Invalid input, please try again"
   end
