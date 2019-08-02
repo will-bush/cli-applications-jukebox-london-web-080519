5303ad836f4d57dca822f023d8bf7cc1aae4c8b2
Will Bush <williamdcbush@gmail.com>
2019-08-02 09:52:32 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -28,7 +28,7 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   selection = gets.strip
-  if selection == 1..9
+  if selection == 1...9
     puts "Playing #{songs[selection -1]}"
   else 
     puts "Invalid input, please try again"
